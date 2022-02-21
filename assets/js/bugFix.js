1a51e0fc0a53f4d67d53c15ab9cf37c6f1c81db5
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-02-21 12:14:45 -0600

    Try to fix capitalize bug

--- assets/js/bugFix.js
+++ assets/js/bugFix.js
@@ -1,8 +1,23 @@
 let links = document.querySelectorAll(".category__link");
 
-for (const link of links) {
+for(const link of links) {
 	console.log(link.innerHTML);
-	link.innerHTML = link.innerHTML.charAt(0).toUpperCase() + link.innerHTML.slice(1);
+	const str = link.innerHTML;
+	let flag = false;
+	if(str.includes("/")) {
+		const arr = str.split("/");
+		flag = true;
+	}
+	else {
+		const arr = str.split(" ");
+	}
+	for (var i = 0; i < arr.length; i++) {
+		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+	}
+
+	if(flag) link.innerHTML = arr.join("/");
+	else link.innerHTML = arr.join(" ");
+	
 	console.log(link.innerHTML);
 }
 
