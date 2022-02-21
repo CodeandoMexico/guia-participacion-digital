cfdb21244a4a5fa7e7a84f4fa907b147dc6674ff
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-02-21 12:09:48 -0600

    Try to fix capitalize bug

--- assets/js/bugFix.js
+++ assets/js/bugFix.js
@@ -3,4 +3,7 @@ let links = document.querySelectorAll(".category__link");
 for (const link of links) {
 	console.log(link.innerHTML);
 	link.innerHTML = link.innerHTML.charAt(0).toUpperCase() + link.innerHTML.slice(1);
+	console.log(link.innerHTML);
 }
+
+console.log("I am running");
