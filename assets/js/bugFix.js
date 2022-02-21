e5df2faeb34b1d718a17937f47bbe2d672d9dd6b
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-02-21 12:06:55 -0600

    Try to fix capitalize bug

--- assets/js/bugFix.js
+++ assets/js/bugFix.js
@@ -1,5 +1,6 @@
 let links = document.querySelectorAll(".category__link");
 
 for (const link of links) {
+	console.log(link.innerHTML);
 	link.innerHTML = link.innerHTML.charAt(0).toUpperCase() + link.innerHTML.slice(1);
 }
