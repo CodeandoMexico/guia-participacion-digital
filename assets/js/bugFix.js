a08e239f58fd9eb4a1447a36585356d16553cd13
BubuDavid <david.pedroza.segoviano@gmail.com>
2022-02-21 12:18:18 -0600

    Try to fix capitalize bug

--- assets/js/bugFix.js
+++ assets/js/bugFix.js
@@ -4,13 +4,12 @@ for(const link of links) {
 	console.log(link.innerHTML);
 	const str = link.innerHTML;
 	let flag = false;
+	let arr = str.split(" ");
 	if(str.includes("/")) {
-		const arr = str.split("/");
+		arr = str.split("/");
 		flag = true;
 	}
-	else {
-		const arr = str.split(" ");
-	}
+
 	for (var i = 0; i < arr.length; i++) {
 		arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
 	}
